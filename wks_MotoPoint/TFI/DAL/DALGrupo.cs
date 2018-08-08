08f116888c2d73984d370fa102c197a6c81b9a7c
angelbrunn <angelbrunn@gmail.com>
2018-08-08 00:29:42 -0300

    AGPB
    Back:Arquitectura Base

--- wks_MotoPoint/TFI/DAL/DALGrupo.cs
+++ wks_MotoPoint/TFI/DAL/DALGrupo.cs
@@ -1,163 +1,154 @@
-﻿using System;
+﻿using SIS.ENTIDAD;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
 
-namespace DAL
+namespace SIS.DATOS
 {
-    class DALGrupo
+    public class DALGrupo
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public List<Grupo> ObtenerGrupos()
+        {
+            List<Grupo> listGrupo = new List<Grupo>();
+            using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["MotoPoint"].ConnectionString))
+            {
+                using (SqlCommand cmdSelect = new SqlCommand("SELECT * FROM tbl_Grupo", con))
+                {
+                    try
+                    {
+                        con.Open();
+                        using (var reader = cmdSelect.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Grupo oGrupo = new Grupo();
+                                oGrupo.IdGrupo = Convert.ToInt32(reader["IdGrupo"]);
+                                oGrupo.grupo = reader["grupo"].ToString();
+                                oGrupo.Descripcion = reader["Descripcion"].ToString();
+                                listGrupo.Add(oGrupo);
+                            }
+                        }
+                        con.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        con.Close();
+                        throw new SIS.EXCEPCIONES.DALExcepcion(ex.Message);
+                    }
+                }
+                return listGrupo;
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="idGrupo"></param>
+        /// <returns></returns>
+        public Grupo ObtenerGrupoPorId(int idGrupo)
+        {
+            Grupo oGrupo = new Grupo();
+            using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["MotoPoint"].ConnectionString))
+            {
+                using (SqlCommand cmdSelect = new SqlCommand("SELECT * FROM tbl_Grupo WHERE idGrupo=@IdGrupo", con))
+                {
+                    try
+                    {
+                        con.Open();
+                        cmdSelect.Parameters.AddWithValue("@IdGrupo", idGrupo);
+                        using (var reader = cmdSelect.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                oGrupo.IdGrupo = Convert.ToInt32(reader["IdGrupo"]);
+                                oGrupo.grupo = reader["grupo"].ToString();
+                                oGrupo.Descripcion = reader["Descripcion"].ToString();
+                            }
+                        }
+                        con.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        con.Close();
+                        throw new SIS.EXCEPCIONES.DALExcepcion(ex.Message);
+                    }
+                }
+                return oGrupo;
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="nombreGrupo"></param>
+        /// <returns></returns>
+        public String ObtenerDescripcionGrupoPorNombreGrupo(String nombreGrupo)
+        {
+            String descripcionGrupo = "Error";
+            using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["MotoPoint"].ConnectionString))
+            {
+                using (SqlCommand cmdSelect = new SqlCommand("SELECT * FROM tbl_Grupo WHERE grupo=@nombreGrupo", con))
+                {
+                    try
+                    {
+                        con.Open();
+                        cmdSelect.Parameters.AddWithValue("@nombreGrupo", nombreGrupo);
+                        using (var reader = cmdSelect.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                descripcionGrupo = reader["grupo"].ToString();
+                            }
+                        }
+                        con.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        con.Close();
+                        throw new SIS.EXCEPCIONES.DALExcepcion(ex.Message);
+                    }
+                }
+                return descripcionGrupo;
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="nombreGrupo"></param>
+        /// <returns></returns>
+        public Grupo obtenerGrupoPorNombreGrupo(String nombreGrupo)
+        {
+            Grupo oGrupo = new Grupo();
+            using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["MotoPoint"].ConnectionString))
+            {
+                using (SqlCommand cmdSelect = new SqlCommand("SELECT * FROM tbl_Grupo WHERE grupo=@nombreGrupo", con))
+                {
+                    try
+                    {
+                        con.Open();
+                        cmdSelect.Parameters.AddWithValue("@nombreGrupo", nombreGrupo);
+                        using (var reader = cmdSelect.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                oGrupo.IdGrupo = Convert.ToInt32(reader["IdGrupo"]);
+                                oGrupo.grupo = reader["grupo"].ToString();
+                                oGrupo.Descripcion = reader["Descripcion"].ToString();
+                            }
+                        }
+                        con.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        con.Close();
+                        throw new SIS.EXCEPCIONES.DALExcepcion(ex.Message);
+                    }
+                }
+                return oGrupo;
+            }
+        }
     }
-
-    /*
-     Public Function obtenerGrupos() As List(Of BE.SIS.ENTIDAD.Grupo)
-            Dim listadoGrupos As New List(Of BE.SIS.ENTIDAD.Grupo)
-
-            Dim conexString As String = System.Configuration.ConfigurationManager.ConnectionStrings("MotoPoint").ConnectionString
-            Dim sqlQuery As String = "SELECT * FROM tbl_Grupo"
-
-            Dim conex As New SqlConnection
-            conex.ConnectionString = conexString
-
-            Dim comando As SqlCommand = conex.CreateCommand
-            comando.CommandType = CommandType.Text
-            comando.CommandText = sqlQuery
-
-            Dim adapter As New SqlDataAdapter(comando)
-            Dim ds As New DataSet
-
-            Try
-                adapter.Fill(ds, "Grupo")
-                Dim enu As IEnumerator(Of DataRow) = ds.Tables("Grupo").Rows.GetEnumerator
-                While enu.MoveNext
-                    Dim oGrupo As New BE.SIS.ENTIDAD.Grupo
-                    oGrupo.idGrupo = CType(enu.Current("idGrupo"), Integer)
-                    oGrupo.grupo = enu.Current("grupo")
-                    oGrupo.descripcion = enu.Current("descripcion")
-                    listadoGrupos.Add(oGrupo)
-                End While
-            Catch
-
-            End Try
-            Return listadoGrupos
-        End Function
-        ''' <summary>
-        ''' 
-        ''' </summary>
-        ''' <param name="idGrupo"></param>
-        ''' <returns></returns>
-        ''' <remarks></remarks>
-        Public Function obtenerGrupoPorId(ByVal idGrupo As Integer) As BE.SIS.ENTIDAD.Grupo
-            Dim oGrupo As New BE.SIS.ENTIDAD.Grupo
-
-            Dim conexString As String = System.Configuration.ConfigurationManager.ConnectionStrings("MotoPoint").ConnectionString
-            Dim sqlQuery As String = "SELECT * FROM tbl_Grupo WHERE idGrupo=@idGrupo"
-
-            Dim conex As New SqlConnection
-            conex.ConnectionString = conexString
-
-            Dim comando As SqlCommand = conex.CreateCommand
-            comando.CommandType = CommandType.Text
-            comando.CommandText = sqlQuery
-
-            Dim iPar As IDataParameter = comando.CreateParameter
-            iPar.ParameterName = "idGrupo"
-            iPar.DbType = DbType.Int32
-            iPar.Value = idGrupo
-            comando.Parameters.Add(iPar)
-
-            Dim adapter As New SqlDataAdapter(comando)
-            Dim ds As New DataSet
-
-            Try
-                adapter.Fill(ds, "Grupo")
-                Dim dr As DataRow = ds.Tables("Grupo").Rows.Item(0)
-                oGrupo.idGrupo = CType(dr("idGrupo"), Integer)
-                oGrupo.grupo = dr("grupo")
-                oGrupo.descripcion = dr("descripcion")
-
-            Catch
-            End Try
-            Return oGrupo
-        End Function
-        ''' <summary>
-        ''' 
-        ''' </summary>
-        ''' <param name="nombreGrupo"></param>
-        ''' <returns></returns>
-        ''' <remarks></remarks>
-        Public Function obtenerDescripcionGrupoPorNombreGrupo(ByVal nombreGrupo As String) As String
-            Dim descripcionGrupo As String = "Error"
-
-            Dim conexString As String = System.Configuration.ConfigurationManager.ConnectionStrings("MotoPoint").ConnectionString
-            Dim sqlQuery As String = "SELECT * FROM tbl_Grupo WHERE grupo=@nombreGrupo"
-
-            Dim conex As New SqlConnection
-            conex.ConnectionString = conexString
-
-            Dim comando As SqlCommand = conex.CreateCommand
-            comando.CommandType = CommandType.Text
-            comando.CommandText = sqlQuery
-
-            Dim iPar As IDataParameter = comando.CreateParameter
-            iPar.ParameterName = "nombreGrupo"
-            iPar.DbType = DbType.String
-            iPar.Value = nombreGrupo
-            comando.Parameters.Add(iPar)
-
-            Dim adapter As New SqlDataAdapter(comando)
-            Dim ds As New DataSet
-
-            Try
-                adapter.Fill(ds, "Grupo")
-                Dim dr As DataRow = ds.Tables("Grupo").Rows.Item(0)
-                descripcionGrupo = dr("descripcion")
-            Catch
-            End Try
-            Return descripcionGrupo
-        End Function
-        ''' <summary>
-        ''' 
-        ''' </summary>
-        ''' <param name="nombreGrupo"></param>
-        ''' <returns></returns>
-        ''' <remarks></remarks>
-        Public Function obtenerGrupoPorNombreGrupo(ByVal nombreGrupo As String) As BE.SIS.ENTIDAD.Grupo
-
-            Dim conexString As String = System.Configuration.ConfigurationManager.ConnectionStrings("MotoPoint").ConnectionString
-            Dim sqlQuery As String = "SELECT * FROM tbl_Grupo WHERE grupo=@nombreGrupo"
-
-            Dim conex As New SqlConnection
-            conex.ConnectionString = conexString
-
-            Dim comando As SqlCommand = conex.CreateCommand
-            comando.CommandType = CommandType.Text
-            comando.CommandText = sqlQuery
-
-            Dim iPar As IDataParameter = comando.CreateParameter
-            iPar.ParameterName = "nombreGrupo"
-            iPar.DbType = DbType.String
-            iPar.Value = nombreGrupo
-            comando.Parameters.Add(iPar)
-
-            Dim adapter As New SqlDataAdapter(comando)
-            Dim ds As New DataSet
-
-            Dim oGrupo As New BE.SIS.ENTIDAD.Grupo
-
-            Try
-                adapter.Fill(ds, "Grupo")
-                Dim dr As DataRow = ds.Tables("Grupo").Rows.Item(0)
-
-                oGrupo.idGrupo = dr("idGrupo")
-                oGrupo.grupo = dr("grupo")
-                oGrupo.descripcion = dr("descripcion")
-
-            Catch
-            End Try
-            Return oGrupo
-        End Function
-     
-    */
 }
--- wks_MotoPoint/TFI/MotoPoint/login.aspx.cs
+++ wks_MotoPoint/TFI/MotoPoint/login.aspx.cs
@@ -16,51 +16,25 @@ namespace MotoPoint
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            SIS.DATOS.DALBitacora dBitacora = new SIS.DATOS.DALBitacora();
+            SIS.DATOS.DALGrupo dDALGrupo = new SIS.DATOS.DALGrupo();
 
-            //TEST 1 ok
-            //var id = dBitacora.ObtenerUltimoId();
+            List<SIS.ENTIDAD.Grupo> listGrupo = new List<SIS.ENTIDAD.Grupo>();
 
-            SIS.ENTIDAD.Bitacora oBitacora = new SIS.ENTIDAD.Bitacora();
-            oBitacora.IdEvento = 5;
-            oBitacora.IdUsuario = "6";
-            oBitacora.Descripcion = "TESTDAL6";
-            oBitacora.Fecha = "1/1/2011";
-            oBitacora.DigitoVerificador = "1111111";
-            //TEST 2 ok
-            //dBitacora.RegistrarEnBitacoraBD(oBitacora);
+            //TEST1
+            listGrupo = dDALGrupo.ObtenerGrupos();
 
-            List<SIS.ENTIDAD.Bitacora> listBitacora = new List<SIS.ENTIDAD.Bitacora>();
-            //TEST 3 ok
-            //listBitacora = dBitacora.ObtenerEventos();
+            SIS.ENTIDAD.Grupo oGrupo1 = new SIS.ENTIDAD.Grupo();
 
-            List<SIS.ENTIDAD.Bitacora> listBitacora2 = new List<SIS.ENTIDAD.Bitacora>();
-            SIS.ENTIDAD.Bitacora oBitacora2 = new SIS.ENTIDAD.Bitacora();
-            SIS.ENTIDAD.Bitacora oBitacora3 = new SIS.ENTIDAD.Bitacora();
+            //TEST2
+            oGrupo1 = dDALGrupo.ObtenerGrupoPorId(1);
 
-            oBitacora2.IdEvento = 1;
-            oBitacora2.IdUsuario = "2";
-            oBitacora2.Descripcion = "AAAAAAAT";
-            oBitacora2.Fecha = "1/1/2012";
-            oBitacora2.DigitoVerificador = "2222222";
+            //TEST3
+            String nombre = dDALGrupo.ObtenerDescripcionGrupoPorNombreGrupo("Admin");
 
-            oBitacora3.IdEvento = 2;
-            oBitacora3.IdUsuario = "3";
-            oBitacora3.Descripcion = "BBBBBBBT";
-            oBitacora3.Fecha = "1/1/2013";
-            oBitacora3.DigitoVerificador = "33333333";
+            SIS.ENTIDAD.Grupo oGrupo2 = new SIS.ENTIDAD.Grupo();
 
-            listBitacora2.Add(oBitacora2);
-            listBitacora2.Add(oBitacora3);
-            //TEST 4 ok
-            dBitacora.InsertarBitacoraDesdeBackup(listBitacora2);
-
-
-            List<SIS.ENTIDAD.Bitacora> listBitacora3 = new List<SIS.ENTIDAD.Bitacora>();
-
-            //TEST 5
-            listBitacora3 = dBitacora.ObtenerTablaBitacora();
-
+            //TEST4
+            oGrupo2 = dDALGrupo.obtenerGrupoPorNombreGrupo("Admin");
 
 
         }
