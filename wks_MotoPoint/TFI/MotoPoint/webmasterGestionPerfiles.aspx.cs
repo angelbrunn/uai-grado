5caf5888bf84955f9d0761d5fc5ebe0829f0960f
Angel Brun <angelbrunn@gmail.com>
2019-01-05 02:06:56 -0300

    AGPB
    D07-Construcción Arquitectura Base

--- wks_MotoPoint/TFI/MotoPoint/webmasterGestionPerfiles.aspx.cs
+++ wks_MotoPoint/TFI/MotoPoint/webmasterGestionPerfiles.aspx.cs
@@ -12,6 +12,7 @@ namespace MotoPoint
     /// </summary>
     public partial class AdminGestionPerfiles : System.Web.UI.Page
     {
+        String lockBusqueda = "0";
         /// <summary>
         /// 
         /// </summary>
@@ -30,8 +31,10 @@ namespace MotoPoint
             else
             {
                 string habilitacionBusqueda = Session["habilitacionBusqueda"].ToString();
+
                 //.....
-                if (habilitacionBusqueda != "0") {
+                if (habilitacionBusqueda == "0" && lockBusqueda == "0")
+                {
                     txtIdUsuario.Enabled = false;
                     txtNombreApellido.Enabled = false;
                     txtFecNac.Enabled = false;
@@ -48,14 +51,27 @@ namespace MotoPoint
         /// <param name="e"></param>
         protected void btnHabilitarBusqueda_Click(object sender, EventArgs e)
         {
+            Session["clickHere"] = System.Convert.ToInt16(Session["clickHere"]) + 1;
+            var numero = System.Convert.ToInt16(Session["clickHere"]);
+            var flag = 0;
 
-            if (txtIdUsuario.Enabled == false)
+            if (numero % 2 == 0 || numero == 1)
             {
+                if (numero != 2)
+                {
+                    flag = 1;
+                }
+            }
+
+            if (txtIdUsuario.Enabled == false && flag == 1)
+            {
+                lockBusqueda = "1";
                 Session["habilitacionBusqueda"] = 1;
                 txtIdUsuario.Enabled = true;
             }
             else
             {
+                lockBusqueda = "0";
                 Session["habilitacionBusqueda"] = 2;
                 txtIdUsuario.Enabled = false;
             }
